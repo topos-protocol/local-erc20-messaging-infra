2cdad68acbfb30b8532a60cc2a4bd29af7afed94
Marko Atanasievski <marko.atanasievski@toposware.com>
2023-12-14 12:18:05 +0100

    fix: review

--- scripts/polygon_edge_healthcheck.sh
+++ scripts/polygon_edge_healthcheck.sh
@@ -4,8 +4,8 @@ set -e
 block_number=$($1/polygon-edge status | grep 'Current Block Number (base 10) = .[0-9]*' | awk '{print $7}')
 
 if [ $block_number -gt 0 ] 
-then 
+then
     exit 0;
 else 
-	exit 1;
+    exit 1;
 fi
