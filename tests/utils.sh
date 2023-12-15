e35b5965113228a4fce328be2d47adcd0f007706
Marko Atanasievski <markoatana@gmail.com>
2023-12-15 13:48:10 +0100

    feat: add certificates delivered test

--- tests/utils.sh
+++ tests/utils.sh
@@ -40,3 +40,10 @@ is_network_running() {
         echo 0
     fi
 }
+
+
+function graphql_get_tce_certificate () {
+    local query='{"query": "{certificate(certificateId: {value: \"'$1'\"})  \n { \n id \n stateRoot \n}}"}'
+    TCE_CERT=$(curl -X POST -H "Content-Type: application/json" -d "$query" $2 | jq -r '.data.certificate.id')
+    echo $TCE_CERT
+}
